52542d38b93e549300fe692c5ea735f1adbfde41
KrlCO <carloscerquinoropeza@gmail.com>
2022-06-10 16:53:54 -0500

    creacion de EspcActivity

---
+++ app/src/main/java/com/example/proyecto_dam1_registro_citas_medicas/EspecActivity.kt
@@ -0,0 +1,11 @@
+package com.example.proyecto_dam1_registro_citas_medicas
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class EspecActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
