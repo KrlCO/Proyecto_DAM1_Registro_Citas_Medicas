3321d810c85323112cbab03dab5a0405c13d83b6
marco antonio zenon aycachi galvan <marco_aycachi@outlook.com>
2022-06-12 14:20:52 -0500

    Modificación de datos

--- app/src/main/java/com/example/proyecto_dam1_registro_citas_medicas/clase/Medico.kt
+++ app/src/main/java/com/example/proyecto_dam1_registro_citas_medicas/clase/Medico.kt
@@ -1,4 +1,9 @@
 package com.example.proyecto_dam1_registro_citas_medicas.clase
 
-class Medico(var cod: Int, var nombre:String, var apellido:String) {
+class Medico(var cod: Int, var nombre:String, var apellido:String
+            , var especialidad: Int, var direccion:String, var correo:String,
+            var telefono:String, var sexo:String, var cid:String) {
+
+
+
 }
